52b69fd5961e02da9930f5af5a5fe6125cab2075
tareqazar <tareq.azar@severalbrands.com>
2023-05-07 14:12:45 +0300

    Enables GraphQL caching, removes checking for ENV variables

--- objectionjs-graphql.js
+++ objectionjs-graphql.js
@@ -1,7 +1,11 @@
 const SchemaBuilder = require('./lib/SchemaBuilder')
 
 module.exports = {
-  builder() {
-    return new SchemaBuilder()
+  // options: ['enableCache', 'cacheTimeout', 'redisKeyPrefix', 'host', 'port']
+  // host and port are for connecting to redis, cacheTimeout is in seconds
+  // redisKeyPrefix is the prefix for all cache keys in Redis
+  // enableCache is a boolean value which indicates whether caching should be enabled or not
+  builder(options = {}) {
+    return new SchemaBuilder(options)
   }
 }
