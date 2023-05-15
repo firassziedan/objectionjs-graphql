0ee1c3a2a08d0d5a18990893133843a233a8c030
tareqazar <108410019+tareqazar@users.noreply.github.com>
2023-05-15 14:14:05 +0300

    Adds light caching (#1)
    * Adds light caching layer
    * Update Readme file 
    ---------
    Authored-by: tareqazar <tareq.azar@severalbrands.com>
    Co-authored-by: Firass Ziedan <firass.ziedan@gmail.com>

--- objectionjs-graphql.js
+++ objectionjs-graphql.js
@@ -1,7 +1,10 @@
 const SchemaBuilder = require('./lib/SchemaBuilder')
 
 module.exports = {
-  builder() {
-    return new SchemaBuilder()
+  // options: ['cacheTimeout', 'redisKeyPrefix', 'host', 'port']
+  // host and port are for connecting to redis, cacheTimeout is in seconds
+  // redisKeyPrefix is the prefix for all cache keys in Redis
+  builder(options) {
+    return new SchemaBuilder(options)
   }
 }
